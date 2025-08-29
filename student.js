704bb633ae38817521722aaf6814598944c024b2
hounseyha1054-wq <hounseyha1054@gmail.com>
2025-08-29 10:58:23 +0700

    Add files via upload
    first

---
+++ student.js
@@ -0,0 +1,14 @@
+const  Students=[
+    {ID:1,NAME:'sey',SEX:'male',Profile:" https://www.esri.com/content/dam/esrisites/en-us/legal/accessibility/assets/resources/accessibility-banner-small-foreground-resources.png",Course:'C++ Program'},
+    {ID:2,NAME:'Sin',SEX:'male',Profile:" https://www.esri.com/content/dam/esrisites/en-us/legal/accessibility/assets/overview/accessibility-banner-medium-foreground-overview.png",Course:'Java Program'},
+    {ID:3,NAME:'Smey',SEX:'male',Profile:"https://www.esri.com/content/dam/esrisites/en-us/arcgis/user-types/assets/user-types-creator-card.jpg",Course:'PHP Program'},
+    {ID:4,NAME:'Sna',SEX:'male',Profile:" https://scontent.fpnh2-2.fna.fbcdn.net/v/t39.30808-6/481190517_1172762821187220_3341195592931429518_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFkzWO69srQLi31PHR23iFJAg1rCDKsV3sCDWsIMqxXe8ECxyvDgXFkS3KYbKJZhM_yv7QPWurJRM0mAlQ6mQpE&_nc_ohc=bqNWVHbHGZcQ7kNvwExgLkv&_nc_oc=AdnryBd5vsGzJRbQvFhpKlHNub1BLtxLQaz9TXwRU420j2vhPoyesRpPK3Y5Uby3DvM&_nc_zt=23&_nc_ht=scontent.fpnh2-2.fna&_nc_gid=RJ3Q_F8xBkUfA3uJ6jVF1g&oh=00_AfL841zxlCstMdqtMx2WIjebJyXRTOirk9qiJmOmn5DSvA&oe=682FCAC5",Course:'Tailwindcss Framework'},
+    {ID:5,NAME:'kilo',SEX:'male',Profile:"https://t4.ftcdn.net/jpg/03/98/85/77/360_F_398857704_n44BPhqM68Xk9vT31BeFkLQwWsgeZS6C.jpg",Course:'Larvel Program'},
+    {ID:6,NAME:'loma',SEX:'male',Profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRRaROhQ1C3f_3XDU290fRAGfd66-CZnjdTMgiApuLcMRuTr5E3txwRy4FfmwLe7hkqpw&usqp=CAU",Course:'Java Script Program'},
+    {ID:7,NAME:'mika',SEX:'male',Profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkAJEkJQ1WumU0hXNpXdgBt9NUKc0QDVIiaw&s",Course:'Python Program'},
+    {ID:8,NAME:'Mami',SEX:'male',Profile:"https://www.smartlook.com/blog/wp-content/uploads/sites/2/2022/01/martin-bolf.png",Course:'Network Program'},
+    {ID:9,NAME:'Mamdo',SEX:'male',Profile:"https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png",Course:'Cyber Security Program'},
+    {ID:10,NAME:'Hala',SEX:'male',Profile:"https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=2723",Course:'Bootstrap Framework'},
+];
+export default Students;
+ 
